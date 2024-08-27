0ce44366e86636f231e12b35a782e71a37e403fc
Akshay Bhutoria <86436079+akshaybhu@users.noreply.github.com>
2024-08-27 20:47:10 +0530

    Update monitor_server.py

--- AssignmentOnPython_28Aug/monitor_server.py
+++ AssignmentOnPython_28Aug/monitor_server.py
@@ -1,15 +1,16 @@
 ## Python program to monitor the health of the CPU
-
 import psutil
-
 
 def monitor_mac_cpu():
     try:
-        cpu_utilization = float(psutil.cpu_percent(interval=1))
-        #print(cpu_utilization)
+        # Calculates the system-wide CPU utilization as a percentage over a 2-second interval.
+        cpu_utilization = psutil.cpu_percent(interval=2)  
+        print('''
+              Do not interrupt.
+              Monitoring CPU usage...
+              ''')
         while True:
-            if cpu_utilization>=80:
-                print("Monitoring CPU usage...")
+            if cpu_utilization>=80:    # comparing current CPU utilization with threashold value provided as 80.
                 print("Alert! CPU usage exceeds threshold: ", cpu_utilization)
     except KeyboardInterrupt:
         print("\nManual interruption by User!!!")
